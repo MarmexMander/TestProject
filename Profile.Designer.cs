9e7701d673768612a865ce41201f3dfd7faaf55e
Marmex Mander <vadim.m.lutsk@gmail.com>
2020-06-04 21:30:42 +0300

    Profile form fix

--- Profile.Designer.cs
+++ Profile.Designer.cs
@@ -114,12 +114,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(182, 284);
+            this.button1.Location = new System.Drawing.Point(82, 287);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(104, 23);
             this.button1.TabIndex = 17;
-            this.button1.Text = "Регистрировать";
+            this.button1.Text = "Вихід";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // fioLabel
             // 
