c46a3befce4ecfe342cd7d042fcf29cb4f354913
kuhnia <colyachepil@gmail.com>
2020-06-07 13:11:44 +0300

    registration

--- SignupForm.cs
+++ SignupForm.cs
@@ -16,5 +16,26 @@ namespace TestProject
         {
             InitializeComponent();
         }
+
+        private bool button1_Click(object sender, EventArgs e)
+        {
+            if (fioBox.Text != "" && addressBox.Text != "" && telNumberBox.Text != "" && emailBox.Text != "" && departmentBox.Text != "" && positionBox.Text != "" && salaryBox.Text != "" && passwordBox.Text != "")
+            {
+
+                if (Database.Users.register(fioBox.Text, false, addressBox.Text, emailBox.Text, positionBox.Text, departmentBox.Text, telNumberBox.Text, Convert.ToSingle(salaryBox.Text), 0, 0, passwordBox.Text))
+                {
+                    return true;
+                }
+                 else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Все поля обязательны.");
+                return false;
+            }
+        }
     }
 }
