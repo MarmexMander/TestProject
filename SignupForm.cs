c54971d08ce7ab8842a5a10b342124b4c7a383ce
Marmex Mander <vadim.m.lutsk@gmail.com>
2020-06-07 18:40:56 +0300

    Fix in SignupForm -> button1_Click

--- SignupForm.cs
+++ SignupForm.cs
@@ -17,24 +17,25 @@ namespace TestProject
             InitializeComponent();
         }
 
-        private bool button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
             if (fioBox.Text != "" && addressBox.Text != "" && telNumberBox.Text != "" && emailBox.Text != "" && departmentBox.Text != "" && positionBox.Text != "" && salaryBox.Text != "" && passwordBox.Text != "")
             {
 
                 if (Database.Users.register(fioBox.Text, false, addressBox.Text, emailBox.Text, positionBox.Text, departmentBox.Text, telNumberBox.Text, Convert.ToSingle(salaryBox.Text), 0, 0, passwordBox.Text))
                 {
-                    return true;
+                    MessageBox.Show("User registered");
+                    this.Close();
                 }
                  else
                 {
-                    return false;
+                    MessageBox.Show("Some error in database");
                 }
             }
             else
             {
                 MessageBox.Show("Все поля обязательны.");
-                return false;
+               
             }
         }
     }
